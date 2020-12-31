8017cdeb1faeeb68c62e339d20d716ff6232a78d
Abderrhim Soubai <gx.coder@gmail.com>
2020-12-31 15:42:50 +0100

    fix(repository): fix import

--- src/main/java/com/octo/repository/VideoRepository.java
+++ src/main/java/com/octo/repository/VideoRepository.java
@@ -4,6 +4,8 @@ package com.octo.repository;
 import com.octo.domain.video.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import java.util.List;
+import com.octo.domain.enums.Level;
 
 public interface VideoRepository extends JpaRepository<Video, String>, JpaSpecificationExecutor<Video> {
 	public List<Video> findByLevelAndTags(Level level, List<String> tags);
