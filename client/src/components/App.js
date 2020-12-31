b12a07d950899948f0b8c025079cd7dae64d2919
Abderrhim Soubai <gx.coder@gmail.com>
2020-12-31 17:25:06 +0100

    fix(client):  last touchs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import Videos from './Video';
 
-const baseURL = "http://localhost:8888/api/v1/videos"
+let baseURL = "http://localhost:8888/api/v1/videos?"
 
 
 
 export default function App() {
     const [videos,setVideos] = useState([])
+    const [tags,setTags] = useState("")
+    const [level,setLevel] = useState("")
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const result = await fetch(baseURL);
-            const data = await result.json()
-            console.log(data)
-            setVideos(data);
-        };
+    const fetchData = async (e) => {
+        e.preventDefault()
+        if(tags){
+            baseURL += `&tags=${tags}`
+        }
+        if(level){
+            baseURL += `&level=${level}`
+        }
+        const result = await fetch(baseURL);
+        const data = await result.json()
+        setVideos(data);
+    };
      
-        fetchData();
-      }, []);
 
-    return <div>
-        <h1>List videos</h1>
-        <Videos videos={videos} />
-    </div> 
+    return <div className="col-3">
+<h2>Search videos</h2>
+<form>
+  <div className="form-group">
+    <label>Level</label>
+    <input className="form-control" value={level} onChange={e => setLevel(e.target.value)} />
+  </div>
+  <div className="form-group">
+    <label>Tags</label>
+    <input className="form-control" value={tags} onChange={e => setTags(e.target.value)} />
+  </div>
+  <button onClick={ e => fetchData(e)} type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+<Videos videos={videos} />
+</div> 
 }
--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 export default function Videos({ video }) {
-    return ( video ? <>
+    return  ( video ? <>
         <h1>{video.title}</h1>
         <p>{video.description}</p>
-        <h4>{video.level}</h4>
         <h4>{video.url}</h4>
     </> : null );
 }
