af55aa33bcf7ebd2a0fcb1392f9e078fcc402c40
BMouhcine <mouhcine.boutinzer@um5s.net.ma>
2021-03-09 18:21:28 +0100

    new branch called 'foo' retrieveVideosByTagAndLevel method implemented

--- src/main/java/com/octo/repository/VideoRepository.java
+++ src/main/java/com/octo/repository/VideoRepository.java
@@ -1,9 +1,14 @@
 package com.octo.repository;
 
 
+import com.octo.domain.enums.Level;
 import com.octo.domain.video.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<Video, String>, JpaSpecificationExecutor<Video> {
+    List<Video> getVideosByTagsAndLevel(List<String> tags, Level level);
+
 }
--- src/main/java/com/octo/services/VideoService.java
+++ src/main/java/com/octo/services/VideoService.java
@@ -1,9 +1,12 @@
 package com.octo.services;
 
 import com.octo.domain.enums.Level;
+import com.octo.domain.video.Video;
 import com.octo.dto.video.VideoDTO;
 import com.octo.mappers.VideoToVideoDTOMapper;
 import com.octo.repository.VideoRepository;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,10 +29,14 @@ public class VideoService {
 
         // TODO code goes here
         // 1. search videos by tags AND/OR level
+        List<Video> videos = videoRepository.getVideosByTagsAndLevel(tags, level);
         // 2. use videoToVideoDTOMapper to map videos to videos DTO
+        List<VideoDTO> videoDTOS = new ArrayList<>();
+        for(Video v: videos)
+            videoDTOS.add(videoToVideoDTOMapper.convert(v));
         // 3 return the list
 
-        return null;
+        return videoDTOS;
     }
 
 
